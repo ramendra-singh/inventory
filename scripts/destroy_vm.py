fb99276cc7944a482fd978c5fddd3053dcffa5b6
ramendra <ramendra@apporbit.com>
2018-03-12 21:54:24 +0530

    Inventory

--- scripts/destroy_vm.py
+++ scripts/destroy_vm.py
@@ -1,92 +1,116 @@
 #!/usr/bin/env python
-"""
-vSphere Python SDK program for shutting down VMs
-"""
+# Copyright 2015 Michael Rice <michael@michaelrice.org>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 from __future__ import print_function
 
-from pyVim.connect import SmartConnect, Disconnect
-from pyVmomi import vim, vmodl
-
-import argparse
 import atexit
-import getpass
-import sys
-import ssl
-import time
+
+from pyVim import connect
+
+from pyVmomi import vim
+
+from tools import cli
+from tools import tasks
 
 
-def GetArgs():
-	"""
-	Supports the command-line arguments listed below.
-	"""
+def setup_args():
 
-	parser = argparse.ArgumentParser(
-		description='Process args for shutting down a Virtual Machine')
-	parser.add_argument('-s', '--host', required=True,
-						action='store', help='Remote host to connect to')
-	parser.add_argument('-o', '--port', type=int, default=443,
-						action='store', help='Port to connect on')
-	parser.add_argument('-u', '--user', required=True, action='store',
-						help='User name to use when connecting to host')
-	parser.add_argument('-p', '--password', required=False,
-						action='store', help='Password to connect to host')
-	parser.add_argument('-v', '--vmname', required=True, action='append',
-						help='Names of the Virtual Machines to shutdown')
-	args = parser.parse_args()
-	return args
+    """Adds additional ARGS to allow the vm name or uuid to
+    be set.
+    """
+    parser = cli.build_arg_parser()
+    # using j here because -u is used for user
+    parser.add_argument('-j', '--uuid',
+                        help='BIOS UUID of the VirtualMachine you want '
+                             'to destroy.')
+    parser.add_argument('-n', '--name',
+                        help='DNS Name of the VirtualMachine you want to '
+                             'destroy.')
+    parser.add_argument('-i', '--ip',
+                        help='IP Address of the VirtualMachine you want to '
+                             'destroy')
+    parser.add_argument('-v', '--vm',
+                        help='VM name of the VirtualMachine you want '
+                             'to destroy.')
 
+    my_args = parser.parse_args()
 
-def main():
-	"""
-   Simple command-line program for shutting down virtual machines on a system.
-   """
-	vmname = ''
-	args = GetArgs()
-	if args.password:
-		password = args.password
-	else:
-		password = getpass.getpass(
-			prompt='Password to host %s and user %s:' % (args.host, args.user))
-	if args.vmname:
-		vmname = args.vmname
-
-	service_instance = None
-	context = ssl._create_unverified_context()
-	context.verify_mode = ssl.CERT_NONE
-
-	try:
-		service_instance = SmartConnect(host=args.host,
-										user=args.user,
-										pwd=args.password,
-										port=int(args.port),
-										sslContext=context)
-		if not service_instance:
-			print("Could not connect to the specified host using specified "
-				  "username and password")
-			return -1
-
-		atexit.register(Disconnect, service_instance)
-
-		content = service_instance.RetrieveContent()
-		# Search for all VMs
-		objview = content.viewManager.CreateContainerView(content.rootFolder,
-														  [vim.VirtualMachine],
-														  True)
-		vmList = objview.view
-		objview.Destroy()
-
-		for vm in vmList:
-			if vm.name in vmname:
-				print("Destroying VM: %s" % vm.name)
-				vm.Destroy_Task()
-
-	except vmodl.MethodFault as error:
-		print("Caught vmodl fault : " + error.msg)
-		return -1
-
-	return 0
+    return cli.prompt_for_password(my_args)
 
 
-# Start program
-if __name__ == "__main__":
-	main()
+def get_obj(content, vimtype, name):
+
+    """Create contrainer view and search for object in it"""
+    obj = None
+    container = content.viewManager.CreateContainerView(
+        content.rootFolder, vimtype, True)
+    for c in container.view:
+        if name:
+            if c.name == name:
+                obj = c
+                break
+        else:
+            obj = c
+            break
+
+    container.Destroy()
+    return obj
+
+ARGS = setup_args()
+SI = None
+try:
+    SI = connect.SmartConnectNoSSL(host=ARGS.host,
+                                   user=ARGS.user,
+                                   pwd=ARGS.password,
+                                   port=ARGS.port)
+    atexit.register(connect.Disconnect, SI)
+except (IOError, vim.fault.InvalidLogin):
+    pass
+
+if not SI:
+    raise SystemExit("Unable to connect to host with supplied credentials.")
+
+VM = None
+if ARGS.vm:
+    VM = get_obj(SI.content, [vim.VirtualMachine], ARGS.vm)
+elif ARGS.uuid:
+    VM = SI.content.searchIndex.FindByUuid(None, ARGS.uuid,
+                                           True,
+                                           False)
+elif ARGS.name:
+    VM = SI.content.searchIndex.FindByDnsName(None, ARGS.name,
+                                              True)
+elif ARGS.ip:
+    VM = SI.content.searchIndex.FindByIp(None, ARGS.ip, True)
+
+if VM is None:
+    raise SystemExit(
+        "Unable to locate VirtualMachine. Arguments given: "
+        "vm - {0} , uuid - {1} , name - {2} , ip - {3}"
+        .format(ARGS.vm, ARGS.uuid, ARGS.name, ARGS.ip)
+        )
+
+print("Found: {0}".format(VM.name))
+print("The current powerState is: {0}".format(VM.runtime.powerState))
+if format(VM.runtime.powerState) == "poweredOn":
+    print("Attempting to power off {0}".format(VM.name))
+    TASK = VM.PowerOffVM_Task()
+    tasks.wait_for_tasks(SI, [TASK])
+    print("{0}".format(TASK.info.state))
+
+print("Destroying VM from vSphere.")
+TASK = VM.Destroy_Task()
+tasks.wait_for_tasks(SI, [TASK])
+print("Done.")
